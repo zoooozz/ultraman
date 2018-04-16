6c4f370b23ada6090086fe7b0b8e4e55f4a9b48a
东流 <liujiebin@laoyuegou.com>
2018-04-16 21:19:58 +0800

    支持启动加端口  PHP cli -h start dev 9999

--- App.php
+++ App.php
@@ -5,11 +5,12 @@
  *
  * @package   ultraman\Foundation
  * @copyright Copyright (c) 2017, ultraman
- * @var 基础服务入口文件 
+ * @var 基础服务入口文件
  */
 
 
 namespace ultraman;
+
 use ultraman\Foundation\DI;
 use League\CLImate\CLImate;
 
@@ -18,7 +19,7 @@ class App
     /**
      * 构造函数 入口加载配置文件进行注入
      *
-     * @param db    数据库配置 
+     * @param db    数据库配置
      * @param main  服务配置项
      * @param api   外部接口配置项
      */
@@ -30,9 +31,9 @@ class App
      */
 
     public function __construct($config = [])
-    {   
+    {
         require dirname(dirname(__FILE__)."/../").'/vendor/autoload.php';
-        if(count($config) == 0){
+        if (count($config) == 0) {
             return true;
         }
         $this->injection($config);
@@ -45,59 +46,62 @@ class App
     public function injection($params)
     {
         foreach ($params as $key => $class) {
-            $f = pathinfo($class, PATHINFO_EXTENSION); 
-            if($f === 'php'){
+            $f = pathinfo($class, PATHINFO_EXTENSION);
+            if ($f === 'php') {
                 $conf = @include $class;
-                if($conf == ""){
+                if ($conf == "") {
                     continue;
                 }
             }
-            if($f === 'ini'){
-                $conf = @parse_ini_file($class,true);
-                if($conf == ""){
+            if ($f === 'ini') {
+                $conf = @parse_ini_file($class, true);
+                if ($conf == "") {
                     continue;
                 }
-            }    
-            DI::set($key,$conf);
+            }
+            DI::set($key, $conf);
         }
         return true;
     }
 
     /**
-     *  服务器启动 
+     *  服务器启动
      */
 
     public function ServiceRun($params)
     {
-
         $this->climate = new CLImate();
         $service = strtolower(trim(isset($params[1])?$params[1]:''));
         $command = strtolower(trim(isset($params[2])?$params[2]:''));
         $env = strtolower(trim(isset($params[3])?$params[3]:''));
+        $port = strtolower(trim(isset($params[4])?$params[4]:''));
+        if ($port) {
+            \ultraman\Foundation\DI::set('port', $port);
+        }
 
-        if(($env == '') || ($service != '-h' && $service!='-t')){
+        if (($env == '') || ($service != '-h' && $service!='-t')) {
             return true;
-        }        
-        $this->configure($env,$command);
-        if($service == '-h'){            
-            if($command == 'start'){
+        }
+        $this->configure($env, $command);
+        if ($service == '-h') {
+            if ($command == 'start') {
                 $app = new \ultraman\Http\HttpYafServer();
             }
-            if($command == 'reload'){
-                $this->reloadSwoole($service,$env);
+            if ($command == 'reload') {
+                $this->reloadSwoole($service, $env);
             }
         }
-        if($service == '-t'){
-            if($command == 'start'){
+        if ($service == '-t') {
+            if ($command == 'start') {
                 $app = new \ultraman\Tcp\SwooleServer($params);
             }
 
-            if($command == 'reload'){
-                $this->reloadSwoole($service,$env);
+            if ($command == 'reload') {
+                $this->reloadSwoole($service, $env);
             }
         }
         
-        return true;        
+        return true;
     }
     
     /**
@@ -105,38 +109,38 @@ class App
      */
 
 
-    private function reloadSwoole($service,$env)
+    private function reloadSwoole($service, $env)
     {
-        $os = strtoupper(substr(PHP_OS,0,3));
-        if($os == 'DAR'){
+        $os = strtoupper(substr(PHP_OS, 0, 3));
+        if ($os == 'DAR') {
             $climate = new climate();
             $climate->error('reload 只支持linux电脑 mac 直接强杀');
-            exit(0);        
+            exit(0);
         }
 
         $config = DI::get('main');
         $class = dirname(dirname(dirname(dirname(__FILE__)))).'/env/'.$env.'/main.ini';
-        $config = @parse_ini_file($class,true);
+        $config = @parse_ini_file($class, true);
         
         $name = $config['common']['application.service_name'];
-        if($name == ''){
+        if ($name == '') {
             return true;
-        }   
+        }
 
-        if($service === '-t'){
+        if ($service === '-t') {
             $name = $name.'tcp';
             $pid = exec('pidof'.' '.$name);
-            if($pid != ''){
+            if ($pid != '') {
                 exec("kill -TERM ".$pid);
                 sleep(1);
                 $app = new \ultraman\Tcp\SwooleServer();
             }
-        }elseif($service === '-h'){
+        } elseif ($service === '-h') {
             $pid = exec('pidof'.' '.$name);
-            if($pid !=''){
-                exec("kill -USR1 ".$pid);   
-            }else{
-                $app = new \ultraman\Http\HttpYafServer();                
+            if ($pid !='') {
+                exec("kill -USR1 ".$pid);
+            } else {
+                $app = new \ultraman\Http\HttpYafServer();
             }
         }
         exit;
@@ -148,23 +152,27 @@ class App
      *  配置文件环境加载
      */
 
-    private function configure($env,$command)
-    {   
+    private function configure($env, $command)
+    {
         $config = dirname(dirname(dirname(dirname(__FILE__))));
         $conf = dir($config.'/conf');
         $env = dir($config.'/env/'.$env);
         
-        while(false != ($item = $conf->read())) {
-            if($item == '.' || $item == '..') continue;
+        while (false != ($item = $conf->read())) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
             unlink($conf->path.'/'.$item);
         }
    
-        while(false != ($item = $env->read())) {
-            if($item == '.' || $item == '..') continue;
-            copy($env->path.'/'.$item,$conf->path.'/'.$item);
+        while (false != ($item = $env->read())) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
+            copy($env->path.'/'.$item, $conf->path.'/'.$item);
             
             $houzhui = substr(strrchr($item, '.'), 1);
-            $keys = basename($item,".".$houzhui);
+            $keys = basename($item, ".".$houzhui);
             $params[$keys] = $conf->path.'/'.$item;
         }
         //加载配置文件
--- Http/HttpServer.php
+++ Http/HttpServer.php
@@ -5,35 +5,40 @@
  */
 
 namespace ultraman\Http;
+
 use ultraman\Foundation\Ecode;
 
 class SwooleHttpServer
 {
-	protected static $_config = [];
-    public function __construct($config) 
-	{
-		if($config['host'] == '' || $config['port'] == ''){
-			throw new Exception("服务器配置错误",Ecode::ERROR);
-		}
-		static::$_config = $config;
-	}
+    protected static $_config = [];
+    public function __construct($config)
+    {
+        $port = \ultraman\Foundation\DI::get('port');
+        if ($port!="") {
+            $config['port'] = $port;
+        }
+        if ($config['host'] == '' || $config['port'] == '') {
+            throw new Exception("服务器配置错误", Ecode::ERROR);
+        }
+        static::$_config = $config;
+    }
 
-	public function connent()
-	{
-		$config = static::$_config;
-		$http = new \swoole_http_server($config['host'], $config['port']);
-		$http->set(
-			array(
-				'worker_num' => $config['worker_num']?:3,
-				'daemonize' => $config['daemonize'],
-	            'max_request' => $config['max_request']?:10000,
-	            'dispatch_mode' => $config['dispatch_mode'],
-	            'task_worker_num'=>$config['task_worker_num']?:4,
-	            'task_ipc_mode'=>$config['task_ipc_mode']?:1,
-	            'task_max_request'=>$config['task_max_request']?:5000,
-	            'log_file' => $config['log_file']
-			)
-		);
-		return $http;
-	}
+    public function connent()
+    {
+        $config = static::$_config;
+        $http = new \swoole_http_server($config['host'], $config['port']);
+        $http->set(
+            array(
+                'worker_num' => $config['worker_num']?:3,
+                'daemonize' => $config['daemonize'],
+                'max_request' => $config['max_request']?:10000,
+                'dispatch_mode' => $config['dispatch_mode'],
+                'task_worker_num'=>$config['task_worker_num']?:4,
+                'task_ipc_mode'=>$config['task_ipc_mode']?:1,
+                'task_max_request'=>$config['task_max_request']?:5000,
+                'log_file' => $config['log_file']
+            )
+        );
+        return $http;
+    }
 }
